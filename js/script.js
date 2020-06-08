6c2a58d19ad3881cc8b6ee65d893d9dc371d716d
Evgeny <sereginevgeny.96@gmail.com>
2020-06-08 15:39:45 +0300

    Это первый комит

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let chooseBtn = document.getElementById('choose'),
+	receiveBtn = document.getElementById('receive'),
+	heading = document.getElementsByTagName('h2')[0],
+	modal = document.querySelector('.modal'),
+	closeBtn = document.querySelector('.close'),
+	nameInput = document.querySelector('.contactform_name'),
+	phoneInput = document.querySelector('.contactform_phone'),
+	mailInput = document.querySelector('.contactform_mail'),
+	text = document.getElementsByName('message')[0];
+
+
+	chooseBtn.addEventListener('mouseenter', function(){
+		heading.textContent = "Действительно все!";
+	});
+
+	chooseBtn.addEventListener('mouseleave', function(){
+		heading.textContent = "Все включено!";
+	});
+
+	receiveBtn.addEventListener('click', function(){
+		modal.style.display = 'block';
+	});
+
+	closeBtn.addEventListener('click', function(){
+		modal.style.display = 'none';
+	});
+
+	nameInput.addEventListener('input', function(){
+		text.value = `Меня зовут ${nameInput.value}. И я хочу просить: `;
+	});
